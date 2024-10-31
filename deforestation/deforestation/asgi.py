9dc3e2bc66ffcc713f813db033609c4d37dec173
Diego Murillo <diegoa31052004@gmail.com>
2024-10-30 22:34:29 -0500

    Feature(configuration): django configuration

---
+++ deforestation/deforestation/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for deforestation project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deforestation.settings')
+
+application = get_asgi_application()
